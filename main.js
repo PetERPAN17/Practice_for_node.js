295b297a13317771cd6e6641596e51a2cc82f000
PetERPAN17 <peterpan17.dev@gmail.com>
2021-01-22 23:28:09 +0900

    Add #2

--- main.js
+++ main.js
@@ -6,34 +6,17 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
+    const readFolder = './data';
 
     if (pathname === '/') {
         if (queryData.id === undefined) {
-            var title = 'Welcome';
-            var description = 'Hello, Node.js';
-            var template = `<!doctype html>
-                    <html>
-                    <head>
-                        <title>WEB1 - ${title}</title>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        <ol>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                        </ol>
-                        <h2>${title}</h2>
-                        <p>${description}</p>
-                    </body>
-                    </html>`;
-            response.writeHead(200);
-            response.end(template);
-        } else {
-            fs.readFile(`data/${queryData.id}`, 'utf-8', (err, description) => {
-                var title = queryData.id;
+            fs.readdir(readFolder, (err, files) => {
+                var list = '';
+                files.forEach(file => {
+                    list += `<li><a href="/?id=${file}">${file}</a></li>`;
+                });
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
                 var template = `<!doctype html>
                     <html>
                     <head>
@@ -43,9 +26,7 @@ var app = http.createServer(function(request,response){
                     <body>
                         <h1><a href="/">WEB</a></h1>
                         <ol>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        ${list}
                         </ol>
                         <h2>${title}</h2>
                         <p>${description}</p>
@@ -54,6 +35,55 @@ var app = http.createServer(function(request,response){
                 response.writeHead(200);
                 response.end(template);
             });
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf-8', (err, description) => {
+                fs.readdir(readFolder, (err, files) => {
+                    var list = '';
+                    files.forEach(file => {
+                        list += `<li><a href="/?id=${file}">${file}</a></li>`;
+                    });
+                    var title = queryData.id;
+                    var template = `<!doctype html>
+                        <html>
+                        <head>
+                            <title>WEB1 - ${title}</title>
+                            <meta charset="utf-8">
+                        </head>
+                        <body>
+                            <h1><a href="/">WEB</a></h1>
+                            <ol>
+                            ${list}
+                            </ol>
+                            <h2>${title}</h2>
+                            <p>${description}</p>
+                        </body>
+                        </html>`;
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
+            // fs.readFile(`data/${queryData.id}`, 'utf-8', (err, description) => {
+            //     var title = queryData.id;
+            //     var template = `<!doctype html>
+            //         <html>
+            //         <head>
+            //             <title>WEB1 - ${title}</title>
+            //             <meta charset="utf-8">
+            //         </head>
+            //         <body>
+            //             <h1><a href="/">WEB</a></h1>
+            //             <ol>
+            //             <li><a href="/?id=HTML">HTML</a></li>
+            //             <li><a href="/?id=CSS">CSS</a></li>
+            //             <li><a href="/?id=JavaScript">JavaScript</a></li>
+            //             </ol>
+            //             <h2>${title}</h2>
+            //             <p>${description}</p>
+            //         </body>
+            //         </html>`;
+            //     response.writeHead(200);
+            //     response.end(template);
+            // });
         }
     } else {
         response.writeHead(404);
